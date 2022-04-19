57883cd316c70227e34444e07a4099dd1da6a335
王峰 <yangdejingjing@gmail.com>
2022-04-19 15:52:41 +0800

    plugin扩展

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,8 +1,24 @@
-export interface BrowserPlugin {
+import { Plugin } from "@capacitor/core";
+
+export interface BrowserPlugin extends Plugin {
   create(options: BrowserOptions): void;
 }
 
 export interface BrowserOptions {
   /** 网页地址 */
-  url: string
+  url: string,
+  /** 主题，dark，light */
+  theme?: string
+}
+
+/**
+ * 分享到微信事件
+ */
+export interface BrowserShareToWechatEventOptions {
+  /** 访问地址 */
+  url: string,
+  /** 缩略图base */
+  thumb: string,
+  /** 标题 */
+  title: string
 }
